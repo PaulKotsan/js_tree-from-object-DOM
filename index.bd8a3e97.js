acee1df65fa5a3e3f428cd656c784cded8fd3d38
Pavlo Kotsan <paul@Pavlos-MacBook-Pro.local>
2024-12-26 15:48:00 -0500

    make build

---
+++ index.bd8a3e97.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("li");if(a.textContent=r,Object.keys(t[r]).length>0){var l=document.createElement("ul");a.appendChild(l),e(l,t[r])}n.appendChild(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.bd8a3e97.js.map
